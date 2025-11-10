c6964c9cea74c30b06a6ac6332f62f3e8edf15a4
Harsh Rajendra Lage <harsh.lage@indexnine.com>
2025-11-10 19:17:18 +0530

    this is the second chagne fo the main branch

--- git.js
+++ git.js
@@ -1,3 +1,11 @@
 funtion onemptied(){
     return 'this is the first funtion i have made '
 }
+
+function two(){
+    return 'this is the second chagne'
+}
+
+// function three(){
+//     return "this is the thrid change "
+// }
